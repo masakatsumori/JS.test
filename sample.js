ce5a35d6127f4f776a9693c9d119d1c70d92192a
森政勝 <morimasakatsu@ms.local>
2023-07-08 16:54:27 +0900

    OBJECT add

--- sample.js
+++ sample.js
@@ -12,3 +12,13 @@ const scores = [1,2,3,4,5,6,7,8,9,10]
 const [gold ,silver , bronze] = scores;
 //gold = scores[0] ...1
 
+
+//OBJECT
+
+const user = {
+    email :"morimori@mori",
+    Id    :"1",
+    pass  :"pass"
+}
+const {email} = user;
+
